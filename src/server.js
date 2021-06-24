a635c5fbc105fc1a86b324e510203259df6fbf23
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-06-23 23:30:47 -0300

    mudando estrutura das pastas css e images

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const route = require('./route');
+const path = require('path')
+
+const server = express();
+
+server.set('view engine', 'ejs') // definindo o ejs como nossa view engine
+
+server.use(express.static("public")) //o express vai usar os css e javascript
+
+server.use(route) //agora o node começou a usar o arquivo route 
+server.set('views', path.join(__dirname, 'views')) // o path pega o caminho da pasta do nosso projeo o join é juntar
+
+server.listen(3000, function() {
+    console.log(`Server is runing in http://localhost:3000`)
+})
