9b782eff50aeaa7509254aa678075b51f3cfd3e7
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-06-24 21:14:06 -0300

    aula 3 metade

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,23 +1,38 @@
+
 import Modal from './modal.js'
 
 const modal = Modal()
 
-// get buttons check
-const checkButtons = document.querySelectorAll('.actions a.check')
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+
+//get the buttons existentes with class check
+const checkButtons = document.querySelectorAll(".actions a.check") 
 
 checkButtons.forEach(button => {
-    // add the listen
-    button.addEventListener("click", event => {
-        modal.open()
-    })
+    //add the listen
+    button.addEventListener("click", handleClick)
 })
 
 
-// Delete button click
+/*When the button delete for clicked it opens to modal */
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
-    button.addEventListener("click", event =>{
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick(event, false))
 })
+
+function handleClick(event, check = true) {
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir"
+    
+
+    modalTitle.innerHTML = `${text} esta pergunta`;
+    modalDescription.innerHTML= `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta`
+    modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+    // open modal
+    modal.open()
+}
--- src/route.js
+++ src/route.js
@@ -6,4 +6,8 @@ routes.get('/', (req, res) => res.render("index"))
 routes.get('/room', (req, res) => res.render("room"))
 routes.get('/create-pass', (req, res) => res.render("create-pass"))
 
+
+// Format that the form within the modal has to pass the information
+// routes.post('/room/:room/:question/:action')
+
 module.exports = routes
